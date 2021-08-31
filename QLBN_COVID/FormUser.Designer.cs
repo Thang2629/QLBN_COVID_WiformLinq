10284d4ad15e898467e5caf366b9fba385e950c5
Diii09 <hoangduy02091999@gmail.com>
2021-08-31 14:34:11 +0700

    form user

--- QLBN_COVID/FormUser.Designer.cs
+++ QLBN_COVID/FormUser.Designer.cs
@@ -29,12 +29,125 @@ namespace QLBN_COVID
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.btnKhoa = new System.Windows.Forms.Button();
+            this.btnXoa = new System.Windows.Forms.Button();
+            this.btnThem = new System.Windows.Forms.Button();
+            this.txtPass = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.lblPlace = new System.Windows.Forms.Label();
+            this.dataUser = new System.Windows.Forms.DataGridView();
+            this.txtUser = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.dataUser)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnKhoa
+            // 
+            this.btnKhoa.Font = new System.Drawing.Font("Times New Roman", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnKhoa.Location = new System.Drawing.Point(547, 161);
+            this.btnKhoa.Name = "btnKhoa";
+            this.btnKhoa.Size = new System.Drawing.Size(95, 28);
+            this.btnKhoa.TabIndex = 20;
+            this.btnKhoa.Text = "Khóa";
+            this.btnKhoa.UseVisualStyleBackColor = true;
+            // 
+            // btnXoa
+            // 
+            this.btnXoa.Font = new System.Drawing.Font("Times New Roman", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnXoa.Location = new System.Drawing.Point(424, 161);
+            this.btnXoa.Name = "btnXoa";
+            this.btnXoa.Size = new System.Drawing.Size(95, 28);
+            this.btnXoa.TabIndex = 19;
+            this.btnXoa.Text = "Sửa";
+            this.btnXoa.UseVisualStyleBackColor = true;
+            // 
+            // btnThem
+            // 
+            this.btnThem.Font = new System.Drawing.Font("Times New Roman", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnThem.Location = new System.Drawing.Point(305, 161);
+            this.btnThem.Name = "btnThem";
+            this.btnThem.Size = new System.Drawing.Size(95, 28);
+            this.btnThem.TabIndex = 18;
+            this.btnThem.Text = "Thêm";
+            this.btnThem.UseVisualStyleBackColor = true;
+            // 
+            // txtPass
+            // 
+            this.txtPass.Location = new System.Drawing.Point(227, 117);
+            this.txtPass.Name = "txtPass";
+            this.txtPass.Size = new System.Drawing.Size(173, 20);
+            this.txtPass.TabIndex = 17;
+            // 
+            // label1
+            // 
+            this.label1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Font = new System.Drawing.Font("Times New Roman", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.label1.Location = new System.Drawing.Point(66, 101);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(74, 17);
+            this.label1.TabIndex = 16;
+            this.label1.Text = "Mật Khẩu";
+            // 
+            // lblPlace
+            // 
+            this.lblPlace.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.lblPlace.AutoSize = true;
+            this.lblPlace.BackColor = System.Drawing.Color.Transparent;
+            this.lblPlace.Font = new System.Drawing.Font("Times New Roman", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblPlace.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.lblPlace.Location = new System.Drawing.Point(66, 45);
+            this.lblPlace.Name = "lblPlace";
+            this.lblPlace.Size = new System.Drawing.Size(76, 17);
+            this.lblPlace.TabIndex = 15;
+            this.lblPlace.Text = "Tài Khoản";
+            // 
+            // dataUser
+            // 
+            this.dataUser.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataUser.Location = new System.Drawing.Point(32, 227);
+            this.dataUser.Name = "dataUser";
+            this.dataUser.Size = new System.Drawing.Size(645, 169);
+            this.dataUser.TabIndex = 14;
+            // 
+            // txtUser
+            // 
+            this.txtUser.Location = new System.Drawing.Point(227, 42);
+            this.txtUser.Name = "txtUser";
+            this.txtUser.Size = new System.Drawing.Size(173, 20);
+            this.txtUser.TabIndex = 13;
+            // 
+            // FormUser
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnKhoa);
+            this.Controls.Add(this.btnXoa);
+            this.Controls.Add(this.btnThem);
+            this.Controls.Add(this.txtPass);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.lblPlace);
+            this.Controls.Add(this.dataUser);
+            this.Controls.Add(this.txtUser);
+            this.Name = "FormUser";
             this.Text = "FormUser";
+            ((System.ComponentModel.ISupportInitialize)(this.dataUser)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button btnKhoa;
+        private System.Windows.Forms.Button btnXoa;
+        private System.Windows.Forms.Button btnThem;
+        private System.Windows.Forms.TextBox txtPass;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label lblPlace;
+        private System.Windows.Forms.DataGridView dataUser;
+        private System.Windows.Forms.TextBox txtUser;
     }
 }
--- QLBN_COVID/FormUser.cs
+++ QLBN_COVID/FormUser.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using QLBN_COVID.DB;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -15,6 +16,39 @@ namespace QLBN_COVID
         public FormUser()
         {
             InitializeComponent();
+        }
+        private DataGridViewRow r;
+        private CovidDataContext db;
+
+
+
+        private void Showdata()
+        {
+
+            var ra = from a in db.User_Logs
+                     select new
+                     {
+                         a.ID,
+                         a.Username,
+                         a.Password,
+                         a.Role,
+                         a.FullName,
+                         a.Status
+                     };
+            dataUser.DataSource = ra;
+        }
+
+        private void FormUser_Load(object sender, EventArgs e)
+        {
+            db = new CovidDataContext();
+            Showdata();
+
+            dataUser.Columns["ID"].HeaderText = "id";
+            dataUser.Columns["Username"].HeaderText = "tai khoan";
+            dataUser.Columns["Password"].HeaderText = "Mat Khau";
+            dataUser.Columns["Role"].HeaderText = "Quyen";
+            dataUser.Columns["FullName"].HeaderText = "ten";
+            dataUser.Columns["Status"].HeaderText = "trang thai";
         }
     }
 }
