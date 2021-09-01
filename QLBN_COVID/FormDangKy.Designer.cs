fbf1d94777172ffd57497c1e280d450799631e1a
Diii09 <hoangduy02091999@gmail.com>
2021-09-01 20:24:17 +0700

    formDK

---
+++ QLBN_COVID/FormDangKy.Designer.cs
@@ -0,0 +1,202 @@
+﻿
+namespace QLBN_COVID
+{
+    partial class FormDangKy
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FormDangKy));
+            this.txtPassWord = new System.Windows.Forms.TextBox();
+            this.txtTen = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.lblPlace = new System.Windows.Forms.Label();
+            this.txtUsername = new System.Windows.Forms.TextBox();
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.btnExist = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // txtPassWord
+            // 
+            this.txtPassWord.Location = new System.Drawing.Point(251, 177);
+            this.txtPassWord.Name = "txtPassWord";
+            this.txtPassWord.Size = new System.Drawing.Size(173, 20);
+            this.txtPassWord.TabIndex = 55;
+            this.txtPassWord.TextChanged += new System.EventHandler(this.txtPassWord_TextChanged);
+            // 
+            // txtTen
+            // 
+            this.txtTen.Location = new System.Drawing.Point(251, 240);
+            this.txtTen.Name = "txtTen";
+            this.txtTen.Size = new System.Drawing.Size(173, 20);
+            this.txtTen.TabIndex = 53;
+            // 
+            // label4
+            // 
+            this.label4.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.label4.AutoSize = true;
+            this.label4.BackColor = System.Drawing.Color.Transparent;
+            this.label4.Font = new System.Drawing.Font("Times New Roman", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label4.Location = new System.Drawing.Point(67, 243);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(86, 17);
+            this.label4.TabIndex = 52;
+            this.label4.Text = "Tên Đầy Đủ";
+            // 
+            // label1
+            // 
+            this.label1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Font = new System.Drawing.Font("Times New Roman", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label1.Location = new System.Drawing.Point(77, 180);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(74, 17);
+            this.label1.TabIndex = 45;
+            this.label1.Text = "Mật Khẩu";
+            // 
+            // lblPlace
+            // 
+            this.lblPlace.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.lblPlace.AutoSize = true;
+            this.lblPlace.BackColor = System.Drawing.Color.Transparent;
+            this.lblPlace.Font = new System.Drawing.Font("Times New Roman", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblPlace.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.lblPlace.Location = new System.Drawing.Point(77, 125);
+            this.lblPlace.Name = "lblPlace";
+            this.lblPlace.Size = new System.Drawing.Size(76, 17);
+            this.lblPlace.TabIndex = 44;
+            this.lblPlace.Text = "Tài Khoản";
+            // 
+            // txtUsername
+            // 
+            this.txtUsername.Location = new System.Drawing.Point(251, 122);
+            this.txtUsername.Name = "txtUsername";
+            this.txtUsername.Size = new System.Drawing.Size(173, 20);
+            this.txtUsername.TabIndex = 42;
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnAdd.BackColor = System.Drawing.Color.Transparent;
+            this.btnAdd.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("btnAdd.BackgroundImage")));
+            this.btnAdd.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.btnAdd.Font = new System.Drawing.Font("Times New Roman", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnAdd.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.btnAdd.Location = new System.Drawing.Point(349, 302);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(75, 30);
+            this.btnAdd.TabIndex = 50;
+            this.btnAdd.Text = "Đăng Ký";
+            this.btnAdd.UseVisualStyleBackColor = false;
+            this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click);
+            // 
+            // label3
+            // 
+            this.label3.BackColor = System.Drawing.Color.Transparent;
+            this.label3.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.label3.Image = ((System.Drawing.Image)(resources.GetObject("label3.Image")));
+            this.label3.Location = new System.Drawing.Point(522, 151);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(64, 64);
+            this.label3.TabIndex = 56;
+            this.label3.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // btnExist
+            // 
+            this.btnExist.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnExist.BackColor = System.Drawing.Color.Transparent;
+            this.btnExist.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("btnExist.BackgroundImage")));
+            this.btnExist.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.btnExist.Font = new System.Drawing.Font("Times New Roman", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnExist.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.btnExist.Location = new System.Drawing.Point(482, 302);
+            this.btnExist.Name = "btnExist";
+            this.btnExist.Size = new System.Drawing.Size(75, 30);
+            this.btnExist.TabIndex = 57;
+            this.btnExist.Text = "Hủy";
+            this.btnExist.UseVisualStyleBackColor = false;
+            // 
+            // label2
+            // 
+            this.label2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.Transparent;
+            this.label2.Font = new System.Drawing.Font("Times New Roman", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label2.Location = new System.Drawing.Point(231, 30);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(218, 26);
+            this.label2.TabIndex = 58;
+            this.label2.Text = "Đăng Ký Tài Khoản";
+            // 
+            // FormDangKy
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoValidate = System.Windows.Forms.AutoValidate.EnablePreventFocusChange;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.ClientSize = new System.Drawing.Size(627, 407);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.btnExist);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtPassWord);
+            this.Controls.Add(this.txtTen);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.btnAdd);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.lblPlace);
+            this.Controls.Add(this.txtUsername);
+            this.ForeColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "FormDangKy";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "FormDangKy";
+            this.Load += new System.EventHandler(this.FormDangKy_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtPassWord;
+        private System.Windows.Forms.TextBox txtTen;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label lblPlace;
+        private System.Windows.Forms.TextBox txtUsername;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button btnExist;
+        private System.Windows.Forms.Label label2;
+    }
+}
---
+++ QLBN_COVID/FormDangKy.cs
@@ -0,0 +1,67 @@
+﻿using QLBN_COVID.DB;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace QLBN_COVID
+{
+    public partial class FormDangKy : Form
+    {
+        public FormDangKy()
+        {
+            InitializeComponent();
+        }
+        private DataGridViewRow r;
+        private CovidDataContext db;
+
+
+        private void showData()
+        {
+            var rs = from u in db.User_Logs
+                     join r in db.User_Roles on u.Role equals r.IDRole
+                     join s in db.User_Status on u.Status equals s.IDStatus
+                     select new
+                     {
+                         u.Username,
+                         u.Password,
+                         u.FullName,
+                         r.Rolename,
+                         s.Statusname
+                     };
+          
+        }
+
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            var tk = new User_Log();
+            tk.Username = txtUsername.Text;
+            tk.Password = txtPassWord.Text;
+            tk.Role = 2;
+            tk.Status = 3;
+            tk.FullName = txtTen.Text;
+            db.User_Logs.InsertOnSubmit(tk);
+
+            db.SubmitChanges();
+            MessageBox.Show("Thêm mới tài khoản thành công!", "Successfully", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            
+            txtUsername.Text = txtPassWord.Text = txtTen.Text = null;
+        }
+
+        private void FormDangKy_Load(object sender, EventArgs e)
+        {
+            db = new CovidDataContext();
+           
+        }
+
+        private void txtPassWord_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
